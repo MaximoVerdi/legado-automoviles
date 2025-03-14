adf827c9a95cd0dd387f0bdf63ef6aed9e4b8628
MaximoVerdi <maximoverdi21@mail.com>
2025-03-14 00:45:58 -0300

    Fix: Responsive

--- src/assets/components/reelSection/reel.jsx
+++ src/assets/components/reelSection/reel.jsx
@@ -3,7 +3,7 @@ import "./reel.css";
 
 function Reel(props) {
     return (
-        <video  autoPlay loop muted>
+        <video autoPlay loop muted playsInline>
             <source src={props.video} type="video/mp4"  />
         </video> 
     )
