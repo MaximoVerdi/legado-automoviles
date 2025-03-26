341ad8960ab092951b129c4f0f9503cf0cf2262b
MaximoVerdi <maximoverdi21@mail.com>
2025-03-26 17:13:13 -0300

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ import { getFirestore } from "firebase/firestore"; // Añade esto
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  authDomain: "legado-automoviles.firebaseapp.com",
   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
